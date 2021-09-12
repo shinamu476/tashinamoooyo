002264795b81d02f6418ccde2bfe8a2177a1ad8c
shinamu476 <shinamu476@gmail.com>
2021-09-12 16:02:05 +0900

    commit from bat

--- svg/loading.js
+++ svg/loading.js
@@ -4,6 +4,6 @@ container: document.getElementById('Lottie'),
 renderer: 'svg', // 描画形式
 loop: true, // trueにしたらループ。1回再生の場合はfalse
 autoplay: true, // 自動再生
-    path: 'https://shinamu476.github.io/tashinamouyo/svg/loading.json', // the path to the animation json
+    path: 'loading.json', // the path to the animation json
     name: "Hello World", // 今後のリファレンスとして名前を指定（オプション）
 });
